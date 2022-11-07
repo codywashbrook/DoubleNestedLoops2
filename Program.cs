0954222ccb65f6170b38b50d90d015a42f220127
codywashbrook <codywashbrook@hotmail.com>
2022-11-07 10:53:00 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoubleNestedLoops2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //2D Arrays
+            //Double Nested Loops
+
+            Console.WriteLine("Double Nested Loops");
+            Console.WriteLine();
+
+            //make a loops inside of another loop
+            //1-10
+            //1-10
+            //inside of inner loop,
+            //
+            for (int i = 0; i <= 9; i++)
+            {
+                for (int j = 0; j <= 9; j++)
+                {
+                    for (int k = 0; k <= 9; k++)
+                    {
+                        Console.WriteLine(i + "" + j + "" + k);
+                    }
+                }
+            }
+
+            Console.WriteLine();
+            Console.ReadKey(true);
+        }
+    }
+}
